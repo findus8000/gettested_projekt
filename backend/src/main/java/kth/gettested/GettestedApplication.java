74c0446a51d16357e80936ebdceb9bbcabacb803
findus8000 <joelpriore@gmail.com>
2024-04-09 10:43:43 +0200

    Initial commit

---
+++ backend/src/main/java/kth/gettested/GettestedApplication.java
@@ -0,0 +1,13 @@
+package kth.gettested;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GettestedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GettestedApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/kth/gettested/GettestedApplicationTests.java
@@ -0,0 +1,13 @@
+package kth.gettested;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GettestedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
