7cdaea8948a406c02af2a1feefe2b4ce3b023093
EhdaaIssa <ehdaa@kth.se>
2024-04-26 07:37:02 +0200

    changes i Landing, fixed bakgrund image

--- frontend/src/pages/landing/Landing.js
+++ frontend/src/pages/landing/Landing.js
@@ -2,16 +2,19 @@ import { Link } from 'wouter';
 import React from "react";
 import './Landing.css';
 import TestButton from "../../components/TestButton";
-
-function Landing(){
+function Landing() {
     return (
         <div id="landingpage">
-            <a>Gettested</a><br/>
-            <div> <img src="getTested-logo-small.png" alt="Gettested Logo"/> </div>
-            <Link href="/statistics">Statistics</Link>
-            <br/>
-            <Link href="/basicStatistics">Old Basic Statistics</Link>
-            <TestButton></TestButton>
+            <div className="content-overlay">
+                <a href="#!">Get tested</a><br/>
+                <div>
+                    <img src="getTested-logo-small.png" alt="Gettested Logo"/>
+                </div>
+                <Link href="/statistics" className="link-button">Statistics</Link>
+                <br/>
+                <Link href="/basicStatistics" className="link-button">Old Basic Statistics</Link>
+                <TestButton/>
+            </div>
         </div>
     );
 }
