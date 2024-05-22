3baea989f457eac92a3a64184c019557eb5baa0d
Dexter Nyika <129272732+DexterEN@users.noreply.github.com>
2024-05-22 17:17:33 +0200

    Added simpel controller init test

--- backend/src/test/java/kth/gettested/GettestedApplicationTests.java
+++ backend/src/test/java/kth/gettested/GettestedApplicationTests.java
@@ -1,13 +1,20 @@
 package kth.gettested;
 
 import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class GettestedApplicationTests {
 
+	@Autowired
+	private MyController controller;
+
 	@Test
-	void contextLoads() {
+	void contextLoads() throws Exception{
+		assertThat(controller).isNotNull();
+
 	}
 
 }
